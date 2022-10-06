1677ceb64445d8c6d53b3b0bd9ca5c6013e3b0d3
Sidecans <sidbhave@hotmail.com>
2022-10-06 16:22:27 -0700

    Update Analyzer_Win11.py

--- Analyzer_Win11.py
+++ Analyzer_Win11.py
@@ -1,3 +1,23 @@
 import os
 var = str(os.getcwd())
 var = var.split("\\")
+drive = var[0]
+if os.path.exists(drive + "\\Program Files\\7-Zip"):
+    print("Software Found: 7-Zip | Igor Pavlov")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Audition 2022"):
+    print("Software Found: Adobe Audition 2022 | Adobe Inc.")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Bridge 2022"):
+    print("Software Found: Adobe Bridge 2022 | Adobe Inc.")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Creative Cloud"):
+    print("Software Found: Adobe Creative Cloud | Adobe Inc.")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Creative Cloud Experience"):
+    print("Software Found: Adobe Creative Cloud | Adobe Inc.")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Illustrator 2022"):
+    print("Software Found: Adobe Illustrator 2022 | Adobe Inc.")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Media Encoder 2022"):
+    print("Software Found: Adobe Media Encoder 2022 | Adobe Inc.")   
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Photoshop 2022"):
+    print("Software Found: Adobe Photoshop 2022 | Adobe Inc.")
+if os.path.exists(drive + "\\Program Files\\Adobe\\Adobe Premiere Pro 2022"):
+    print("Software Found: Adobe Premiere Pro 2022 | Adobe Inc.")
+
